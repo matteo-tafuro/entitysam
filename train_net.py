cdbf6af7231c7b60670d3f456edcea234ff46ac8
ymq2017 <949286448@qq.com>
2025-07-13 19:13:44 +0200

    training code

--- train_net.py
+++ train_net.py
@@ -1,3 +1,7 @@
+# Copyright (c) Adobe EntitySAM team.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
 
 import copy
 import itertools
